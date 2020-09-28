ac97ffed080f556c234dbfef3bf0be9be0768566
davidgo123 <david.ernst20@googlemail.com>
2020-09-28 22:27:08 +0100

    cozy

--- HueWorkAnimation/model/HueWorkScript.py
+++ HueWorkAnimation/model/HueWorkScript.py
@@ -6,18 +6,18 @@ bridge_ip = "192.168.178.45"
 bridge_username = "J5RVHpoWwgviHQbnYdPaYHJTe63sHIRQxVs1ja6i"
 
 # ID der Hueplay
-group_id = 4
+group_id_bars = 4
 
 
 def work():
         payload = {"on":True, "bri": 250, "xy":[0.3227,0.329]}
         headers = {'content-type': 'application/json'}
-        r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
+        r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(payload), headers=headers)
 
 def chill():
         payload = {"on":True, "bri":200, "xy":[0.5,0.35]}
         headers = {'content-type': 'application/json'}
-        r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
+        r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(payload), headers=headers)
 
 
 while True:
--- HueWorkAnimation/model/WebServer.py
+++ HueWorkAnimation/model/WebServer.py
@@ -12,14 +12,15 @@ bridge_ip = "192.168.178.45"
 bridge_username = "J5RVHpoWwgviHQbnYdPaYHJTe63sHIRQxVs1ja6i"
 headers = {'content-type': 'application/json'}
 # ID der Hueplay
-group_id = 4
+group_id_bars = 4
+group_id_lights = 6
 # Variable fuer Aktive Konzentrationspausen
 work_light = False
 
 # Webserver
 model = Flask(__name__)
 
-# Arbeitsskript
+# Arbeits-Skript
 @model.route("/work")
 def work():
 	global work_light
@@ -29,8 +30,8 @@ def work():
 
 	if work_light:
 		Start_HUE()
-		process_work = subprocess.Popen(['python', 'model/LampControl.py'])
-		process_av = subprocess.Popen(['python', 'model/AVstart.py'])
+		process_work = subprocess.Popen(['python', 'model/HueWorkScript.py'])
+		process_av = subprocess.Popen(['python', 'model/AV-Control.py'])
 
 	elif not work_light:
 		if process_work:
@@ -38,20 +39,38 @@ def work():
 		End_HUE()
 	return str(work_light)
 
-# Chillskript (Bett)
+# Chill-Skript
 @model.route("/chill")
 def chill():
 	global work_light
 	global process_work
 	global headers
 
-	work_light = False;
+	work_light = False
 
 	if process_work:
 		process_work.terminate()
 
 	chill = {"on":True, "bri":250, "xy":[0.48,0.42]}
-	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(chill), headers=headers)
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(chill), headers=headers)
+	return str(work_light)
+
+# Cozy-Skript
+@model.route("/cozy")
+def cozy():
+	global work_light
+	global process_work
+	global headers
+
+	work_light = False
+
+	if process_work:
+		process_work.terminate()
+
+	cozy_bars = {"on":True, "bri":100, "xy":[0.57,0.35]}
+	cozy_lights = {"on":True, "bri":50, "xy":[0.57,0.35]}
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(cozy_bars), headers=headers)
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(cozy_lights), headers=headers)
 	return str(work_light)
 
 # invertieren eines boolischen Wertes
@@ -63,7 +82,7 @@ def Start_HUE():
 	global headers
 
 	green = {"on":True, "bri": 200, "xy":[0.17,0.7]}
-	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(green), headers=headers)
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(green), headers=headers)
 	time.sleep(0.5)
 
 # Feedback beim beenden des Arbeitsskriptes
@@ -71,11 +90,10 @@ def End_HUE():
 	global headers
 
 	red = {"on":True, "bri": 200, "xy":[0.7,0.3]}
-	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(red), headers=headers)
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(red), headers=headers)
 	time.sleep(0.8)
 	chill = {"on":True, "bri":250, "xy":[0.48,0.42]}
-	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(chill), headers=headers)
-
+	r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id_bars)+"/action", data=json.dumps(chill), headers=headers)
 
 # Webserver aufruf
 if __name__ == "__main__":

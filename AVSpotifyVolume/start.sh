f987f8439dd2a9fbb6c67db3e81b069e6eb4e0ca
davidgo123 <david.ernst20@googlemail.com>
2020-09-29 14:52:47 +0100

    Delete double folder

--- AVSpotifyVolume/start.sh
+++
@@ -1,3 +0,0 @@
-docker build -t av-spotify-volume .
-
-docker run -d --restart unless-stopped av-spotify-volume
--- HueWorkAnimation/start.sh
+++
@@ -1,4 +0,0 @@
-docker build -t web-server .
-
-docker run -d --restart unless-stopped --name web-server -v$PWD/model:/web-server -p 192.168.178.50:1337:1337 web-server
-

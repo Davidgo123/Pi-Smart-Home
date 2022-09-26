f9c2ec4a93b500ffbc76ff9526c6b0d6d297081a
David <david.ernst20@gmail.com>
2022-09-26 21:39:35 +0200

    Heizungssteuerung

--- Heizungssteuerung/Heizungssteuerung.py
+++ Heizungssteuerung/Heizungssteuerung.py
@@ -5,7 +5,7 @@ from fritzconnection.lib.fritzhosts import FritzHosts
 
 macs = ['12:5E:5E:87:E7:9A',  # David
         '1E:6A:E8:18:00:99',  # Lukas
-        '16:D1:B3:39:7E:C4'   # Yannick
+        '16:D1:B3:39:7E:C4'  # Yannick
         ]
 
 ADDRESS = '192.168.178.1'
@@ -14,6 +14,11 @@ PASSWORD = 'hirt3846'
 currentTemp = 0
 oldTemp = 0
 
+# state for last Check if one Device is home
+DeviceWasHomeState = False
+DeviceIsHomeState = False
+
+
 # ---------------------------------------------------------------
 
 def getCurrentWeatherAsJson():
@@ -21,16 +26,18 @@ def getCurrentWeatherAsJson():
     r = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=52,333&lon=9,7&units=metric&appid=662e86ccdebdefe4deafe7379cd9113a')
     return json.loads(r.text)
 
+
 # round down currentTemp and return
 def getCurrentTemp():
     json_data = getCurrentWeatherAsJson()
-    return json_data['main']['temp']//1
+    return json_data['main']['temp'] // 1
 
 
 # ---------------------------------------------------------------
 
-# Abfragen des Power Zustandes
-def isDeviceHome(devices):
+def checkIfDeviceIsHome(devices):
+    global DeviceIsHomeState
+
     # Get connected devices in network
     fh = FritzHosts(address='192.168.178.1', password='hirt3846')
     hosts = fh.get_hosts_info()
@@ -42,55 +49,91 @@ def isDeviceHome(devices):
         # Check Online Status
         for device in devices:
             if device == mac:
-                return True
+                DeviceIsHomeState = True
+                return
 
     # return False if no Device is Online
-    return False
+    DeviceIsHomeState = False
+    return
 
 
 # ---------------------------------------------------------------
 
-# check parameters
-while True:
+def checkDeviceConstraint():
+    global DeviceIsHomeState
+    global DeviceWasHomeState
 
-    # get current temp
-    currentTemp = getCurrentTemp()
+    # check if no one is home
+    if not DeviceIsHomeState:
+        # check if heating is not already off
+        r = requests.get('http://192.168.178.106/relay/0?')
+        if not r.json()['ison']:
+            # stop heating
+            requests.post('http://192.168.178.106/relay/0?turn=off')
+
+    # check temp if someone comes home
+    if DeviceIsHomeState and not DeviceWasHomeState:
+        if checkTempConstraint_ON():
+            requests.post('http://192.168.178.106/relay/0?turn=on')
 
 
-    for x in range(30):
-        # check stop heating (device constraint)
-        if not isDeviceHome(macs):
-            # check if heating is not already off
-            r = requests.get('http://192.168.178.106/relay/0?')
-            if not r.json()['ison']:
-                # stop heating
-                r = requests.post('http://192.168.178.106/relay/0?turn=off')
-        time.sleep(60)
+# ---------------------------------------------------------------
 
-    # check stop heating (temp constraint)
+# check start heating (temp constraint)
+def checkTempConstraint_ON():
+    global currentTemp
+    global oldTemp
+
+    if currentTemp <= 12 and oldTemp > 12:
+        print("Temp goes under 13°, check devices!")
+        # check if heating is not already on
+        r = requests.get('http://192.168.178.106/relay/0?')
+        if r.json()['ison']:
+            return True
+    return False
+
+
+# check stop heating (temp constraint)
+def checkTempConstraint_OFF():
+    global currentTemp
+    global oldTemp
+
     if currentTemp > 12 and oldTemp <= 12:
         print("Temp goes over 12°, heating stopped!")
         # check if heating is not already off
         r = requests.get('http://192.168.178.106/relay/0?')
         if not r.json()['ison']:
-            # stop heating
-            r = requests.post('http://192.168.178.106/relay/0?turn=off')
-
-    # check start heating (temp constraint)
-    if currentTemp <= 12 and oldTemp > 12:
-        print("Temp goes under 13°, check devices!")
-
-        if isDeviceHome(macs):
-            print("Online device found, start heating!")
-            # check if heating is not already on
-            r = requests.get('http://192.168.178.106/relay/0?')
-            if r.json()['ison']:
-                # start heating
-                r = requests.post('http://192.168.178.106/relay/0?turn=on')
+            return True
+    return False
 
 
-    # save temp from now
-    oldTemp = currentTemp
+# ---------------------------------------------------------------
 
-    # wait one minute
-    time.sleep(60)
+try:
+
+    # main loop
+    while True:
+        # get current temp
+        currentTemp = getCurrentTemp()
+        checkIfDeviceIsHome(macs)
+
+        # check 30 times device constraint (one time per minute)
+        for x in range(30):
+            checkDeviceConstraint()
+            time.sleep(60)
+
+        if checkTempConstraint_OFF():
+            requests.post('http://192.168.178.106/relay/0?turn=off')
+
+        if checkTempConstraint_ON() and DeviceIsHomeState:
+            requests.post('http://192.168.178.106/relay/0?turn=on')
+
+        # save temp from now
+        oldTemp = currentTemp
+        DeviceWasHomeState = DeviceIsHomeState
+
+        # wait one minute
+        time.sleep(60)
+
+except:
+    print("An exception occurred")
